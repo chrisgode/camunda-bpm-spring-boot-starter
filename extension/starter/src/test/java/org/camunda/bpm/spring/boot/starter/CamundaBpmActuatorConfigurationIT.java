1e9a44232029910631d2eb2d6193207cb95a50ea
Oliver Steinhauer <oliver.steinhauer@gmail.com>
2016-09-13 09:39:14 +0200

    cleanup tests

--- extension/starter/src/test/java/org/camunda/bpm/spring/boot/starter/CamundaBpmActuatorConfigurationIT.java
+++ extension/starter/src/test/java/org/camunda/bpm/spring/boot/starter/CamundaBpmActuatorConfigurationIT.java
@@ -2,20 +2,17 @@ package org.camunda.bpm.spring.boot.starter;
 
 import static org.junit.Assert.assertTrue;
 
-import org.camunda.bpm.spring.boot.starter.test.TestActuatorApplication;
+import org.camunda.bpm.spring.boot.starter.test.TestApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = { TestActuatorApplication.class }, properties = {
-    "management.security.enabled:false" }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext
+@SpringBootTest(classes = { TestApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CamundaBpmActuatorConfigurationIT {
 
   @Value("${local.server.port}")
